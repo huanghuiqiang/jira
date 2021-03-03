5fb54c8e084b0c1b9740bb422f99a74b66e66ae5
二零九八 <huanghuiqiang86@gmail.com>
2021-03-03 20:03:36 +0800

    feat: add useMount  Hook and useDebounce Hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -13,25 +13,47 @@ export const PorjextListScreen = () => {
   });
   const [list, setList] = useState([])
   const [users, setUsers] = useState([])
+  const useDebounceParam = useDebounce(param, 1000);
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response=> {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(useDebounceParam))}`).then(async response=> {
       if (response.ok) {
         setList(await response.json())
       }
     })
-  }, [param])
+  }, [useDebounceParam])
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if (response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
 
   return <div>
     <SearchPanel users={users} param={param} setParam={setParam} />
     <List users={users} list={list}/>
   </div>
+}
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+}
+
+const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 在Value变化时，新设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个use Effect运行后执行。清理上一个定时器
+    return () => clearTimeout(timeout)
+  }, [value, delay])
+
+  // 返回最新的值
+  return debouncedValue;
 }
